78c23ca7eb12da2946f0a971a2b03ab5e5776d25
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-04-29 14:03:51 -0700

    update: made start of db model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+JarTypes=['Servers', 'Proxies', 'Modded', 'Vanilla', 'Bedrock']
+SoftwareTypes=[
+  'Vanilla',
+  'Paper',
+  'Purpur',
+  'Spigot',
+  'CraftBukkit',
+  'Tuinity',
+  'Airplane',
+  'Folia',
+  'SpongeVanilla',
+  'Glowstone',
+  'Fabric',
+  'Forge',
+  'Crucible',
+  'Cardboard',
+  'Arclight',
+  'Mohist',
+  'Magma',
+  'SpongeForge',
+  'Waterfall',
+  'Velocity',
+  'Bungeecord',
+  'Travertine',
+  'GeyserMC',
+  'TunnelMC']
+
+class jars:
+  type=models.CharField(choices=JarTypes)
+  version=models.CharField(max_length=50)
+  software=models.CharField(choices=SoftwareTypes)
+  
