2a8ea731cf224f0318ce32e4ac748cdf68a2cd07
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-04-29 23:08:05 -0700

    update: removed model from api as it is voided by manager

--- api/models.py
+++ api/models.py
@@ -1,11 +1,1 @@
 from django.db import models
-
-class jars(models.Model):
-  type = models.CharField(max_length=300)
-  version = models.CharField(max_length=20)
-  build_number = models.IntegerField()
-  commit = models.IntegerField()
-
-
-
-
