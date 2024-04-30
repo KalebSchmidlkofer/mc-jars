aa666a044f453a2f13d48a9ec37eadc3f058d7ba
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-04-29 18:51:23 -0700

    update: mostly complete models

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import jars, servers, bedrock, modded, vanilla, proxies
 
-# Register your models here.
+
+class jar_admin(admin.ModelAdmin):
+  verbose_name='Jars'
+  list_display = ('version', 'software', 'date_added')
+
+
+
+admin.site.register(jars, jar_admin)
+
+admin.site.register(servers)
+admin.site.register(bedrock)
+admin.site.register(modded)
+admin.site.register(vanilla)
+admin.site.register(proxies)
--- frontend/models.py
+++ frontend/models.py
@@ -1,34 +1,62 @@
 from django.db import models
+from os import path
+Types = [
+  ('Servers', 'Servers'),
+  ('Proxies', 'Proxies'),
+  ('Modded', 'Modded'),
+  ('Vanilla', 'Vanilla'),
+  ('Bedrock', 'Bedrock')
+]
 
-JarTypes=['Servers', 'Proxies', 'Modded', 'Vanilla', 'Bedrock']
-SoftwareTypes=[
-  'Vanilla',
-  'Paper',
-  'Purpur',
-  'Spigot',
-  'CraftBukkit',
-  'Tuinity',
-  'Airplane',
-  'Folia',
-  'SpongeVanilla',
-  'Glowstone',
-  'Fabric',
-  'Forge',
-  'Crucible',
-  'Cardboard',
-  'Arclight',
-  'Mohist',
-  'Magma',
-  'SpongeForge',
-  'Waterfall',
-  'Velocity',
-  'Bungeecord',
-  'Travertine',
-  'GeyserMC',
-  'TunnelMC']
+SoftwareTypes = [
+  ('Vanilla', 'Vanilla'),
+  ('Paper', 'Paper'),
+  ('Purpur', 'Purpur'),
+  ('Spigot', 'Spigot'),
+  ('CraftBukkit', 'CraftBukkit'),
+  ('Tuinity', 'Tuinity'),
+  ('Airplane', 'Airplane'),
+  ('Folia', 'Folia'),
+  ('SpongeVanilla', 'SpongeVanilla'),
+  ('Glowstone', 'Glowstone'),
+  ('Fabric', 'Fabric'),
+  ('Forge', 'Forge'),
+  ('Crucible', 'Crucible'),
+  ('Cardboard', 'Cardboard'),
+  ('Arclight', 'Arclight'),
+  ('Mohist', 'Mohist'),
+  ('Magma', 'Magma'),
+  ('SpongeForge', 'SpongeForge'),
+  ('Waterfall', 'Waterfall'),
+  ('Velocity', 'Velocity'),
+  ('Bungeecord', 'Bungeecord'),
+  ('Travertine', 'Travertine')
+]
 
-class jars:
-  type=models.CharField(choices=JarTypes)
+class jars(models.Model):
+  title = models.CharField(max_length=50, unique=True, default=None)
+  type=models.CharField(choices=Types, max_length=50)
   version=models.CharField(max_length=50)
-  software=models.CharField(choices=SoftwareTypes)
-  
+  software=models.CharField(choices=SoftwareTypes, max_length=50)
+  buildnum=models.IntegerField()
+  posted=models.BooleanField(default=True)
+  file=models.FileField(upload_to=f'')
+  date_added=models.DateTimeField(auto_now=False, auto_now_add=True)
+
+
+class servers(models.Model):
+  type=models.ForeignKey(jars, on_delete=models.CASCADE, related_name='Server')
+
+class bedrock(models.Model):
+  type=models.ForeignKey(jars, on_delete=models.CASCADE, related_name='Bedrock')
+
+class modded(models.Model):
+  type=models.ForeignKey(jars, on_delete=models.CASCADE, related_name='Modded')
+
+class vanilla(models.Model):
+  type=models.ForeignKey(jars, on_delete=models.CASCADE, related_name='Vanilla')
+
+class proxies(models.Model):
+  type=models.ForeignKey(jars, on_delete=models.CASCADE, related_name='Proxy')
+
+
