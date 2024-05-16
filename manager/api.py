b5e88c15f23eaa4879dd25d42c757a3e88a3b48b
kaleb <kaleb@hibiki.ashbyte.com>
2024-05-16 14:49:52 -0700

    update: im not testing all of those yet, but they should work

--- manager/api.py
+++ manager/api.py
@@ -1,6 +1,6 @@
 # fastapi_app/api.py
 from fastapi import FastAPI, UploadFile, File, APIRouter
-from .views import uploadtoServer
+from .views import uploadtoModded, uploadtoServer, uploadtoOther, uploadtoProxy, uploadtoVanilla, uploadtoBedrock
 from asgiref.sync import sync_to_async
 import shutil
 
@@ -25,4 +25,62 @@ async def uploadServer(
   await sync_to_async(uploadtoServer)(file=file,filecontent=contents, project=project, version=version, build=build)
   return {"filename": file.filename}
 
+@app.post('/upload/modded')
+async def uploadModded(
+  file: UploadFile,
+  project: str,
+  version: str,
+  build: int = 0,
+):
+  contents = await file.read()
 
+  await sync_to_async(uploadtoModded)(file=file,filecontent=contents, project=project, version=version, build=build)
+  return {"filename": file.filename}
+
+@app.post('/upload/vanilla')
+async def uploadVanilla(
+  file: UploadFile,
+  project: str,
+  version: str,
+  build: int = 0,
+):
+  contents = await file.read()
+
+  await sync_to_async(uploadtoVanilla)(file=file,filecontent=contents, project=project, version=version, build=build)
+  return {"filename": file.filename}
+
+@app.post('/upload/bedrock')
+async def uploadBedrock(
+  file: UploadFile,
+  project: str,
+  version: str,
+  build: int = 0,
+):
+  contents = await file.read()
+
+  await sync_to_async(uploadtoBedrock)(file=file,filecontent=contents, project=project, version=version, build=build)
+  return {"filename": file.filename}
+
+@app.post('/upload/proxy')
+async def uploadProxy(
+  file: UploadFile,
+  project: str,
+  version: str,
+  build: int = 0,
+):
+  contents = await file.read()
+
+  await sync_to_async(uploadtoProxy)(file=file,filecontent=contents, project=project, version=version, build=build)
+  return {"filename": file.filename}
+
+@app.post('/upload/misc')
+async def uploadMisc(
+  file: UploadFile,
+  project: str,
+  version: str,
+  build: int = 0,
+):
+  contents = await file.read()
+
+  await sync_to_async(uploadtoOther)(file=file,filecontent=contents, project=project, version=version, build=build)
+  return {"filename": file.filename}
--- manager/views.py
+++ manager/views.py
@@ -1,25 +1,69 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import jar, servers, modded, vanilla, bedrock, proxies, other
+from .models import SoftwareTypes, jar, servers, modded, vanilla, bedrock, proxies, other
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-def uploadtoServer(file, filecontent, project, version, build):
+
+def jarInstance(file, filecontent, project, version, build, software):
   file = SimpleUploadedFile(f'{project}-{version}-{build}.jar', filecontent)
   jarInstance = jar.objects.create(
     title=f'{project}-{version}',
     project=project,
     version=version,
-    software='Servers',
+    software=software,
     buildnum=build,
     posted=True,  
     experimental=False, 
     file=file  
   )
-
-  serverInstance = servers.objects.create(
-    project=jarInstance
-  )
   jarInstance.save()
+  return jarInstance
+
+def uploadtoServer(file, filecontent, project, version, build):
+  Instance=jarInstance(file, filecontent, project, version, build, 'Servers')
+  
+  serverInstance = servers.objects.create(
+    project=Instance
+  )
   serverInstance.save()
 
+def uploadtoModded(file, filecontent, project, version, build):
+  Instance=jarInstance(file, filecontent, project, version, build, 'Modded')
+  
+  serverInstance = modded.objects.create(
+    project=Instance
+  )
+  serverInstance.save()
+
+def uploadtoProxy(file, filecontent, project, version, build):
+  Instance=jarInstance(file, filecontent, project, version, build, 'Proxies')
+  
+  serverInstance = proxies.objects.create(
+    project=Instance
+  )
+  serverInstance.save()
+
+def uploadtoVanilla(file, filecontent, project, version, build):
+  Instance=jarInstance(file, filecontent, project, version, build, 'Vanilla')
+  
+  serverInstance = vanilla.objects.create(
+    project=Instance
+  )
+  serverInstance.save()
+
+def uploadtoBedrock(file, filecontent, project, version, build):
+  Instance=jarInstance(file, filecontent, project, version, build, 'Bedrock')
+  
+  serverInstance = bedrock.objects.create(
+    project=Instance
+  )
+  serverInstance.save()
+
+def uploadtoOther(file, filecontent, project, version, build):
+  Instance=jarInstance(file, filecontent, project, version, build, 'Misc')
+  
+  serverInstance = other.objects.create(
+    project=Instance
+  )
+  serverInstance.save()
 
