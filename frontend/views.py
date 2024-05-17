1f1d8226befc2404168ee1919f6447f795104ab4
kaleb <kaleb@hibiki.ashbyte.com>
2024-05-16 20:48:57 -0700

    update: started working on proper templates and switching computers

--- frontend/views.py
+++ frontend/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def go_home(request):
-  return HttpResponse("Hello, World!")
+  return render(request, 'frontend/home.html')
--- jars/settings.py
+++ jars/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
